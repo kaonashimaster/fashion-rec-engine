32d693d6307257c8b994c487df4244c8cc2a9fb3
kaonashimaster <a.hisa06nori05tamon@gmail.com>
2026-04-16 16:26:11 +0900

    storesも除法取れなかったのでbaseに変更

--- stores_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://mi-na-mo.stores.jp/"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-}
-
-response = requests.get(url, headers=headers)
-print(response.text[:3000])
